8f25d5fe69463a79a9a65c72427a403d374634ed
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-02-16 18:58:43 +0300

    Update Contents.swift

--- yandex2022.playground/Contents.swift
+++ yandex2022.playground/Contents.swift
@@ -1,6 +1,24 @@
 // Task 1
 // Дана строка (UTF-8). Найти самый часто встречаюшийся в ней символ. Если несколько символов встречаются одинаково часто, то можно вывести любой.
-let str = "ababa"
+let str = "ababacsf"
 var char = Array(str)
 var simbol = ""
 var count = 0
+
+var charSet = Set(char)
+var newCharSet = Array(charSet)
+
+for element in newCharSet {
+    var elementCount = 0
+    for j in 0...char.count - 1 {
+        if char[j] == element {
+            elementCount += 1
+        }
+    }
+    if elementCount > count {
+        count = elementCount
+        simbol = "\(element)"
+    }
+}
+
+print("Simbol: \(simbol), count: \(count)")
