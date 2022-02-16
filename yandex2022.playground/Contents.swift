445caf384246be1b83dacd6f89b17b3bf59765ed
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-02-16 17:21:48 +0300

    Solution O(N2)

--- yandex2022.playground/Contents.swift
+++ yandex2022.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+// Task 1
+// Дана строка (UTF-8). Найти самый часто встречаюшийся в ней символ. Если несколько символов встречаются одинаково часто, то можно вывести любой.
+let oneString = "abababbbccccccc"
+let chars = Array(oneString)
+var strElement = ""
+var count = 0
 
-var greeting = "Hello, playground"
+for i in 0...chars.count - 1 {
+    var simbolCount = 0
+    for j in 0...chars.count - 1 {
+        if chars[i] == chars[j] {
+            simbolCount += 1
+        }
+    }
+    if simbolCount > count {
+        count = simbolCount
+        strElement = "Simbol \(chars[i]), count = \(simbolCount)"
+    }
+    
+}
+print(strElement)
