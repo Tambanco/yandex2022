b23c2797d790c85ffa7c2e1722b5f8f8d8d07184
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-02-17 14:50:09 +0300

    Update LinearSearch.playground/Contents.swift
    not tested

--- LinearSearch.playground/Contents.swift
+++ LinearSearch.playground/Contents.swift
@@ -1,1 +1,19 @@
+// Дана последовательность чисел длинной N
+// Найти первое (левое) вхождение положительного числа Х в неё или вывести -1, если число Х не встречалось.
 
+let seq = [1,4,5,7,8,9]
+var idx = -1
+
+func searcher(sequence: [Int], x: Int) -> String {
+    
+    guard x >= 0 else { return "Vvedite pologitelnoe chislo"}
+    
+    for i in 0...sequence.count - 1 {
+        if sequence[i] == x && idx == -1 {
+            idx = i
+        }
+     }
+    return "pervii index chisla \(x) = \(idx)"
+}
+
+searcher(sequence: seq, x: 1)
