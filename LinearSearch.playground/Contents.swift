f1c2a94344501dcd952d697df8a0908e83ef430c
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-02-17 15:11:41 +0300

    Update Contents.swift
    add tests

--- LinearSearch.playground/Contents.swift
+++ LinearSearch.playground/Contents.swift
@@ -1,11 +1,18 @@
 // Дана последовательность чисел длинной N
 // Найти первое (левое) вхождение положительного числа Х в неё или вывести -1, если число Х не встречалось.
 
-let seq = [1,4,5,7,8,9]
+let seq1 = [1,4,5,7,8,9,56,33]
 var idx = -1
 
+//TESTS
+let seqIsEmpty: [Int] = []
+let seqContainsOne = [2]
+let seqXIsOnFirstIndex = [2,4,5,7,8,9]
+let seqXIsOnLastIndex = [3,4,5,7,8,2]
+let seqAllNegative = [-1,-4,-6,-8]
+
 func searcher(sequence: [Int], x: Int) -> String {
-    
+    guard sequence.isEmpty == false else { return "-1"}
     guard x >= 0 else { return "Vvedite pologitelnoe chislo"}
     
     for i in 0...sequence.count - 1 {
@@ -13,7 +20,7 @@ func searcher(sequence: [Int], x: Int) -> String {
             idx = i
         }
      }
-    return "pervii index chisla \(x) = \(idx)"
+    return "\(idx)"
 }
 
-searcher(sequence: seq, x: 1)
+searcher(sequence: seq1, x: 9)
