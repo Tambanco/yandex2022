1babae3858d3432ad5e6b01355e70964a32a2a70
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-03-13 18:26:24 +0300

    Update Contents.swift

--- Part3.playground/Contents.swift
+++ Part3.playground/Contents.swift
@@ -1,1 +1,5 @@
+// Задача №6
+// Дана последовательность слов. Вывести все самые короткие слова через пробел
+
+let seq = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]
 
